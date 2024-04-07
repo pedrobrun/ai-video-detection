b7bea1a11bf6eb1267728c21db7f33c7a74d355a
Pedro Brun <pedroluccavb@gmail.com>
2024-04-07 20:54:11 -0300

    feat: add cors

--- ai_model/app.py
+++ ai_model/app.py
@@ -5,11 +5,14 @@ from dotenv import load_dotenv
 import os
 from flask_migrate import Migrate
 from flask_executor import Executor
+from flask_cors import CORS
 
 load_dotenv()
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
+
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 
     db.init_app(app)
@@ -19,7 +22,6 @@ def create_app():
     model = OnnxService("yolov8s")
 
     executor = Executor(app)
-
 
     app.model = model
     app.executor = executor
