1ef34bfb9dffbd6f13664dc79c1905fd276c284a
Pedro Brun <pedroluccavb@gmail.com>
2024-04-07 20:22:37 -0300

    feat: add timestamps on video

--- ai_model/models/video.py
+++ ai_model/models/video.py
@@ -1,6 +1,9 @@
 from database.connection import db
+from datetime import datetime
 
 class Video(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
-    video_data = db.Column(db.LargeBinary, nullable=False)  # Storing video as BLOB
+    video_data = db.Column(db.LargeBinary, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.now())
+    updated_at = db.Column(db.DateTime, nullable=False, default=datetime.now(), onupdate=datetime.now())
