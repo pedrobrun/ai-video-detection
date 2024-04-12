23e2c362d4b1c11f464be22a2bfb439435899a5c
Pedro Brun <pedroluccavb@gmail.com>
2024-04-12 02:10:40 -0300

    refactor: extract configs to a config class

--- ai_model/app.py
+++ ai_model/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from config import Config
 from services.jobs import update_processing_detections
 from services.onnx import OnnxService
 from database.connection import db
@@ -9,48 +10,39 @@ from flask_executor import Executor
 from flask_cors import CORS
 from flask_apscheduler import APScheduler
 
-scheduler = APScheduler()
-
-load_dotenv()
-
 def create_app():
+    load_dotenv()
     app = Flask(__name__)
+    app.config.from_object(Config)
     CORS(app)
-
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
-    app.app_context().push()
     
     db.init_app(app)
-    # db.create_all()
+    Migrate(app, db)
 
-    migrate = Migrate(app, db)
+    setup_scheduler(app)
+    setup_executor(app)
 
-    model = OnnxService("yolov8s")
-
-    app.config['EXECUTOR_TYPE'] = 'thread'
-    app.config['EXECUTOR_MAX_WORKERS'] = 4
-
-    executor = Executor(app)
-
-    app.model = model
-    app.executor = executor
-
-    with app.app_context():
-        from routes import main
-        app.register_blueprint(main)
-
-        # Wrapper function that sets up the application context for the job
-        def job_wrapper():
-            with app.app_context():
-                update_processing_detections()
-
-        scheduler.add_job(id='Update Idle Processing Detections',
-                          func=job_wrapper,
-                          trigger='interval', minutes=1)
-        scheduler.start()
+    from routes import main
+    app.register_blueprint(main)
 
     return app
 
+def setup_scheduler(app):
+    scheduler = APScheduler()
+    scheduler.init_app(app)
+
+    def job_wrapper():
+        with app.app_context():
+            update_processing_detections()
+
+    scheduler.add_job(id='Update Idle Processing Detections', func=job_wrapper, trigger='interval', minutes=1)
+    scheduler.start()
+
+def setup_executor(app):
+    executor = Executor(app)
+    app.executor = executor
+    app.model = OnnxService("yolov8n")
+
 if __name__ == "__main__":
     app = create_app()
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=app.config['DEBUG'])
---
+++ ai_model/config.py
@@ -0,0 +1,8 @@
+import os
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    EXECUTOR_TYPE = 'thread'
+    EXECUTOR_MAX_WORKERS = 4
+    DEBUG = True
