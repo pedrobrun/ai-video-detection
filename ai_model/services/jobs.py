e42faa465c8193f1e3ef6988d36846aec91e5914
Pedro Brun <pedroluccavb@gmail.com>
2024-04-11 22:25:54 -0300

    feat: adjusting prediction status

--- ai_model/services/jobs.py
+++ ai_model/services/jobs.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timedelta
+from sqlalchemy import func
 from database.connection import db
 from flask import current_app as app
 from enums.detection import DetectionStatus
@@ -6,16 +7,17 @@ from models.prediction import Prediction
 from models.detection import Detection
 
 def update_processing_detections():
-    ten_minutes_ago = datetime.now() - timedelta(minutes=10)
+    ten_minutes_ago = datetime.now() - timedelta(minutes=60)
     
-    # Query for detections that are PROCESSING and have not had a new prediction in the last 10 minutes
-    detections_to_update = Detection.query.filter(
-        Detection.status == DetectionStatus.PROCESSING,
-        Detection.predictions.any(Prediction.created_at < ten_minutes_ago) | ~Detection.predictions.any()
+    # Query for detections that are PROCESSING and have not had a new prediction in the last 30 minutes
+    detections_to_update = Detection.query.join(Prediction, isouter=True).filter(
+        Detection.status == DetectionStatus.PROCESSING
+    ).group_by(Detection.id).having(
+        func.max(Prediction.created_at) < ten_minutes_ago if Prediction.created_at is not None else True
     ).all()
-    
+
     for detection in detections_to_update:
-        detection.status = DetectionStatus.STOPPED
-        app.logger.info(f"Updated detection {detection.id} to STOPPED.")
+        detection.status = DetectionStatus.IDLE
+        app.logger.info(f"Updated detection {detection.id} to IDLE.")
     
     db.session.commit()
