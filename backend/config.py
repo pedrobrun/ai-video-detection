5417ebe270f34ef82bd3605e0cac385520a90517
Pedro Brun <pedroluccavb@gmail.com>
2024-04-12 15:57:52 -0300

    feat: adjust executor configs

--- backend/config.py
+++ backend/config.py
@@ -3,6 +3,6 @@ import os
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    EXECUTOR_TYPE = 'thread'
-    EXECUTOR_MAX_WORKERS = 4
+    EXECUTOR_TYPE = 'process'
+    EXECUTOR_MAX_WORKERS = 6
     DEBUG = True
