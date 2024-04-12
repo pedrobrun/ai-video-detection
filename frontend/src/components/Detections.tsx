7213dcac3041aa801ac5bf75b5e229d377734a35
Pedro Brun <pedroluccavb@gmail.com>
2024-04-11 22:44:50 -0300

    feat: ui tweaks

--- frontend/src/components/Detections.tsx
+++ frontend/src/components/Detections.tsx
@@ -2,13 +2,20 @@
 
 import { api } from '@/api'
 import { cn, getStatusColor } from '@/lib/utils'
-import { Detection, Paginated } from '@/types'
+import { Detection, DetectionStatus, Paginated } from '@/types'
 import Link from 'next/link'
 import { useEffect, useState } from 'react'
 import { Spinner } from './Spinner'
 import { Button } from './ui/button'
 
-export function Detections({}: {}) {
+const statusDescriptions = {
+  PROCESSING: 'Currently generating the predictions.',
+  FAILED: 'Detection failed due to an error.',
+  SUCCESS: 'All frames processed successfully.',
+  IDLE: 'No activity detected for 60 minutes, may be incomplete because of a system outage which prevented all predictions from finishing.'
+};
+
+export function Detections({ }: {}) {
   const [data, setData] = useState<
     (Paginated & { detections: Detection[] }) | undefined
   >()
@@ -33,15 +40,26 @@ export function Detections({}: {}) {
   }, [currentPage])
 
   return (
-    <div>
+    <div className='flex flex-col items-center'>
+      <div className="flex flex-col max-w-[300px] justify-center items-center gap-2 mb-4">
+        <div>Status Meanings:</div>
+        <div className='flex flex-col items-start'>
+          {Object.entries(statusDescriptions).map(([status, description]) => (
+            <div key={status} className="flex items-baseline gap-1">
+              <span className={cn('min-h-3 min-w-3 rounded-full', getStatusColor(status as DetectionStatus, 'bg'))} />
+              <span className="text-sm">{`${status}: ${description}`}</span>
+            </div>
+          ))}
+        </div>
+      </div>
       <div className="w-[300px] p-1 flex flex-col gap-4 bg-opacity-20 overflow-y-auto">
-      {data &&
+        {data &&
           data.detections &&
-          data.detections.length > 0 && 
+          data.detections.length > 0 &&
           <div className='flex justify-center w-full'>
             <Button disabled={isLoading} onClick={fetchDetections} className='mt-2'>Refresh</Button>
           </div>
-          }
+        }
         {isLoading ? (
           <div className="flex w-full justify-center">
             <Spinner />
@@ -75,7 +93,7 @@ export function Detections({}: {}) {
                     <strong>{detection.model_name}</strong>
                   </p>
                   <p className="text-sm truncate overflow-hidden w-full">
-                    <span className="opacity-50">Model</span>:{' '}
+                    <span className="opacity-50">Status</span>:{' '}
                     <strong>{detection.status}</strong>
                   </p>
                 </div>
