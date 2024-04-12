1f78152ba918ade3385b77e03945315822c7ae75
Pedro Brun <pedroluccavb@gmail.com>
2024-04-12 10:28:07 -0300

    refactor: improve readability on background job for detections

--- backend/services/jobs.py
+++ backend/services/jobs.py
@@ -7,17 +7,27 @@ from models.prediction import Prediction
 from models.detection import Detection
 
 def update_processing_detections():
-    ten_minutes_ago = datetime.now() - timedelta(minutes=60)
+    stale_threshold = datetime.now() - timedelta(minutes=60)
     
-    # Query for detections that are PROCESSING and have not had a new prediction in the last 30 minutes
-    detections_to_update = Detection.query.join(Prediction, isouter=True).filter(
+    # Construct a query to find all detections that are still marked as PROCESSING
+    # but haven't received a new prediction in the last X minutes
+    query = Detection.query.join(
+        Prediction, isouter=True
+    ).filter(
         Detection.status == DetectionStatus.PROCESSING
-    ).group_by(Detection.id).having(
-        func.max(Prediction.created_at) < ten_minutes_ago if Prediction.created_at is not None else True
-    ).all()
+    ).group_by(
+        Detection.id
+    )
+
+    query = query.having(
+        func.coalesce(func.max(Prediction.created_at), datetime.min) < stale_threshold
+    )
+
+    detections_to_update = query.all()
 
     for detection in detections_to_update:
         detection.status = DetectionStatus.IDLE
         app.logger.info(f"Updated detection {detection.id} to IDLE.")
     
     db.session.commit()
+
